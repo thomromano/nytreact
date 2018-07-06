9549d4e09b916f2ec43d1121216f4e16043ba0ff
thomromano <thomromano@gmail.com>
2018-07-06 16:11:49 -0500

    rudimentary react app complete

---
+++ routes/api/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const articleRoutes = require("./articles");
+
+console.log("articles api routes included");
+
+router.use("/articles", articleRoutes);
+
+module.exports = router;
