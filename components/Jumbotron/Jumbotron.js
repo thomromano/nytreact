d03efafd7f8083ed0aa9636d7441eb4e29d767b8
thomromano <thomromano@gmail.com>
2018-07-09 19:22:02 -0500

    add components

---
+++ components/Jumbotron/Jumbotron.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Jumbotron = () =>
+  <div className="jumbotron text-center">
+    <h2>New York Times Article Scraperr</h2>
+    <h3>Search and save articles.</h3>
+  </div>;
+
+export default Jumbotron;
